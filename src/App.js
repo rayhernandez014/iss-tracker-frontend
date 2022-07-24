428e8361d246fb4f6bd0731d534ac4a0531cd3cc
Ray Hernandez <rayhernandez014@gmail.com>
2022-07-23 23:52:27 -0400

    now frontend requests location to backend and not to api; api key is not used in the frontend anymore

--- src/App.js
+++ src/App.js
@@ -3,7 +3,6 @@ import locationService from './services/location'
 import Map from './components/Map'
 import Bar from './components/Bar'
 import Drawer from './components/Drawer'
-import authService from './services/auth'
 import Container from '@mui/material/Container'
 import HourglassTopTwoToneIcon from '@mui/icons-material/HourglassTopTwoTone'
 import Box from '@mui/material/Box'
@@ -14,22 +13,12 @@ function App() {
   const [location, setLocation] = useState(null)
 
   useEffect( () => {
-    initialize()
-  }, [])
-
-  const initialize = async () => {
-    await getKey()
     getInfo()
-  }
+  }, [])
 
   const getInfo = async () => {
     const result = await locationService.getLocation()
-    setLocation(result)
-  }
-
-  const getKey = async () => {
-    const result = await authService.getKey()
-    locationService.confKey(result.key)
+    setLocation(result.location)
   }
 
   const theme = createTheme({
--- src/services/location.js
+++ src/services/location.js
@@ -1,24 +1,12 @@
 import axios from 'axios'
 
-const url = 'https://uphere-space1.p.rapidapi.com/satellite/25544/location'
-
-const params = { units: 'metric' }
-
-const headers = {
-  'X-RapidAPI-Key': null,
-  'X-RapidAPI-Host': 'uphere-space1.p.rapidapi.com'
-}
-
-const confKey = (key) => {
-  headers['X-RapidAPI-Key'] = key
-}
+const baseUrl = '/api/location'
 
 const getLocation = async () => {
-  const config = { params, headers }
-  const response = await axios.get(url, config)
+  const response = await axios.get(baseUrl)
   return response.data
 }
 
-const locationService = { getLocation, confKey }
+const locationService = { getLocation }
 
 export default locationService
